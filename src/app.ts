9ad9f7b5973324561c60e013d042489819da60ed
Muslim-Abubakr <ismusis@yandex.ru>
2024-05-11 15:52:19 +0300

    added homework

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import { addressesRoute } from './routes/addresses-route'
+import { productsRoute } from './routes/products-route'
+
+export const app = express()
+const port = 3020
+
+app.use(express.json())
+
+app.use('/products', productsRoute)
+
+app.use('/addresses', addressesRoute)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-import express, { Request, Response} from 'express'
-import { addressesRoute } from './routes/addresses-route'
-import { productsRoute } from './routes/products-route'
+import { app } from "./app"
+import { SETTINGS } from "./settings"
 
-const app = express()
-const port = 3020
-
-app.use(express.json())
-
-app.use('/products', productsRoute)
-
-app.use('/addresses', addressesRoute)
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(SETTINGS.PORT, () => {
+  console.log('...server started')
 })
